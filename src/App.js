f877c4b5133ce4595d68f84774096b7a0862f01e
DESKTOP-6RTUS3H\EDWIN <edwinshi_work@yeah.net>
2022-08-21 11:27:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.scss";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="left">left</div>
+        <div className="right">right</div>
+        
+        <div className="clear"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
