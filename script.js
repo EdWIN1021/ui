01bebf6c89e5e52c1de90cb9831f666aaf5dc161
Edwin <>
2023-11-06 10:09:40 -0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const panels = document.querySelectorAll(".panel");
+const circles = document.querySelectorAll(".circle");
 
-panels.forEach((panel) =>
-  panel.addEventListener("click", () => {
-    removeAllActives();
-    panel.classList.add("active");
-  })
-);
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.getElementById("next");
 
-const removeAllActives = () => {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
-};
+const progress = document.querySelector(".progress");
+
+let current = 0;
+let currWidth = 32;
+
+nextBtn.addEventListener("click", () => {
+  if (current < circles.length - 1) {
+    current++;
+    progress.style.width = currWidth * current + "%";
+    circles[current].classList.add("active");
+  }
+
+  if (current === circles.length - 1) {
+    nextBtn.disabled = true;
+  }
+
+  if (current > 0) {
+    prevBtn.disabled = false;
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (current > 0) {
+    circles[current].classList.remove("active");
+    current--;
+    progress.style.width = currWidth * current + "%";
+  }
+
+  if (current === 0) {
+    prevBtn.disabled = true;
+  }
+
+  if (current < circles.length - 1) {
+    nextBtn.disabled = false;
+  }
+});
